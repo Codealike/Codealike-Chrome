7a9ae5f960c3f8b9a5d0f04ab1d3c9dd6d7da986
David Messinger <dmessing@users.noreply.github.com>
2023-02-15 11:26:07 -0500

    delted all

--- src/background/browser-api/windows.ts
+++
@@ -1,8 +0,0 @@
-export const getFocusedWindowId = async () => {
-  const windows = await chrome.windows.getAll();
-
-  return (
-    windows.find((window) => window.focused)?.id ||
-    chrome.windows.WINDOW_ID_NONE
-  );
-};
--- src/shared/messages.ts
+++
@@ -1,3 +0,0 @@
-export const WAKE_UP_BACKGROUND = 'wake-up';
-export const LIMIT_EXCEEDED = 'limit-exceeded';
-export const LIMIT_OK = 'limit-ok';
