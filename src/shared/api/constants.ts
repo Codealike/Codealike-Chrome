ab1e0dfb40e65b0dc823972d2404a84935f0b3c4
Harman <harmandeep.singh@randstaddigital.com>
2025-06-03 11:59:11 +0530

    feat: add development API URL support

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require('../../../package.json');
+const isProduction = process.env.NODE_ENV === 'production'
 
-export const CodealikeHost = 'https://codealike.com/api/v2';
+export const CodealikeHost = (isProduction ? 'https://codealike.com/api/v2' : 'https://dev.codealike.com/api/v2');
 export const InvalidTokenError = 'Invalid token provided';
 
 export const CurrentClientVersion = packageJson.version;
